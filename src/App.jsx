b3600917c7c3d0b9a918b702c4dbb163ff37beb0
adityaexe <kmradi2002@gmail.com>
2024-08-25 02:51:44 +0530

    remove progress section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from "./Component/About/About";
 import Hero from "./Component/Hero/Hero";
 import Navbar from "./Component/Navbar/Navbar";
 import Slidera from "./Component/Slider/Slider";
-import Test from "./Test";
 
 function App() {
   return (
@@ -14,9 +13,6 @@ function App() {
         <Slidera />
       </div>
       <About />
-      <div className="flex justify-center items-center h-screen bg-gray-100 p-6">
-        <Test />
-      </div>
     </>
   );
 }
